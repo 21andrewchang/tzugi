b92cd4d7b8536063ba5f9f6e3be366f78f7266e2
Andrew Chang <21andrewch@gmail.com>
2024-03-31 21:50:24 -0500

    feat: modals work

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import {
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Modal from "react-native-modal";
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [isVisible, setVisible] = useState(false);
+  const toggleModal = () => {
+    setVisible(!isVisible);
+  };
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back,
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleModal}>
+            <Text style={styles.text}>Transactions</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={toggleModal}>
+            <Text style={styles.text}>Reciepts</Text>
+          </TouchableOpacity>
+          <Modal animationType="slide" isVisible={isVisible}>
+            <SafeAreaView className="flex-1">
+              <TouchableOpacity onPress={toggleModal}>
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </SafeAreaView>
+          </Modal>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -12,8 +67,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
   },
 });
