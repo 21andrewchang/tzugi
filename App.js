4f453c563ca5e6d21bf9fa09cd348b49e8299bad
Andrew Chang <21andrewch@gmail.com>
2024-04-01 07:55:19 -0500

    feat: sort of sketchy blur

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Camera, CameraType } from "expo-camera";
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, TouchableOpacity } from "react-native";
+import { BlurView } from "expo-blur";
 import Navigation from "./src/Navigation";
 import Transactions from "./src/Transactions";
 import Reciepts from "./src/Reciepts";
@@ -9,14 +10,25 @@ export default function App() {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [recieptsModal, setRecieptsModal] = useState(false);
   const [transactionsModal, setTransactionsModal] = useState(false);
+  const [blur, setBlur] = useState(0);
 
   const reciept1 = require("./assets/testReciept.png");
   const recieptImages = [reciept1, reciept1, reciept1]; // Just using the same receipt for example, you can replace with your array of receipts
 
   const toggleReciepts = () => {
+    if (!recieptsModal) {
+      setBlur(100);
+    } else {
+      setBlur(0);
+    }
     setRecieptsModal(!recieptsModal);
   };
   const toggleTransactions = () => {
+    if (!transactionsModal) {
+      setBlur(100);
+    } else {
+      setBlur(0);
+    }
     setTransactionsModal(!transactionsModal);
   };
 
@@ -39,10 +51,12 @@ export default function App() {
   return (
     <View className="flex-1 justify-center">
       <Camera className="flex-1" type={CameraType.back}>
-        <Navigation
-          toggleTransactions={toggleTransactions}
-          toggleReciepts={toggleReciepts}
-        />
+        <BlurView intensity={blur} tint="light" className="flex-1">
+          <Navigation
+            toggleTransactions={toggleTransactions}
+            toggleReciepts={toggleReciepts}
+          />
+        </BlurView>
         <Transactions
           toggleTransactions={toggleTransactions}
           transactionsModal={transactionsModal}
--- src/Reciepts.js
+++ src/Reciepts.js
@@ -8,6 +8,7 @@ import {
   View,
 } from "react-native";
 import Modal from "react-native-modal";
+import { BlurView } from "expo-blur";
 
 export default function Reciepts({
   recieptImages,
@@ -15,42 +16,48 @@ export default function Reciepts({
   toggleReciepts,
 }) {
   return (
-    <Modal backdropOpacity={0.9} animationType="fade" isVisible={recieptsModal}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <TouchableOpacity
-          style={{ position: "absolute", top: 10, left: 10 }}
-          onPress={toggleReciepts}
-        >
-          <Text style={{ color: "white", padding: 10 }}>Close</Text>
-        </TouchableOpacity>
-        <ScrollView style={{ flex: 1, marginTop: 20 }}>
-          {recieptImages.map((reciept, index) => (
-            <View
-              key={index}
-              style={{ flexDirection: "row", marginBottom: 20 }}
-            >
-              <Image
-                source={reciept}
-                style={{
-                  flex: 1,
-                  marginRight: 10,
-                  aspectRatio: 1,
-                  resizeMode: "contain",
-                }}
-              />
-              <Image
-                source={reciept} // Using the same receipt for demonstration, you can replace with your actual image source
-                style={{
-                  flex: 1,
-                  marginLeft: 10,
-                  aspectRatio: 1,
-                  resizeMode: "contain",
-                }}
-              />
-            </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Modal>
+    <BlurView intensity={0} tint="dark">
+      <Modal
+        backdropOpacity={0.9}
+        animationType="fade"
+        isVisible={recieptsModal}
+      >
+        <SafeAreaView style={{ flex: 1 }}>
+          <TouchableOpacity
+            style={{ position: "absolute", top: 10, left: 10 }}
+            onPress={toggleReciepts}
+          >
+            <Text style={{ color: "white", padding: 10 }}>Close</Text>
+          </TouchableOpacity>
+          <ScrollView style={{ flex: 1, marginTop: 20 }}>
+            {recieptImages.map((reciept, index) => (
+              <View
+                key={index}
+                style={{ flexDirection: "row", marginBottom: 20 }}
+              >
+                <Image
+                  source={reciept}
+                  style={{
+                    flex: 1,
+                    marginRight: 10,
+                    aspectRatio: 1,
+                    resizeMode: "contain",
+                  }}
+                />
+                <Image
+                  source={reciept} // Using the same receipt for demonstration, you can replace with your actual image source
+                  style={{
+                    flex: 1,
+                    marginLeft: 10,
+                    aspectRatio: 1,
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+            ))}
+          </ScrollView>
+        </SafeAreaView>
+      </Modal>
+    </BlurView>
   );
 }
