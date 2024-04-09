d7d280c9feec3052ed96040634b6156fb8d4de52
Andrew Chang <21andrewch@gmail.com>
2024-04-08 21:21:36 -0500

    feat: transaction bar color change

--- src/components/TransactionBar.js
+++ src/components/TransactionBar.js
@@ -5,15 +5,15 @@ export default function TransactionBar({ index, transaction }) {
   return (
     <View className="flex-row my-1 h-14" key={index}>
       <TouchableOpacity className="w-14" onPress={() => {}}>
-        <View className="justify-center items-center h-full rounded-l-lg bg-black/50">
+        <View className="justify-center items-center h-full rounded-l-lg bg-gray-500/30">
           <Image source={recieptIcon} className="w-3 h-4" />
         </View>
       </TouchableOpacity>
 
       <TouchableOpacity className="flex-1" onPress={() => {}}>
         <View className="flex-row items-center h-full bg-black rounded-r-lg">
-          <Text className="ml-4 text-white text-m">{transaction.price}</Text>
-          <Text className="ml-4 text-m text-white/40">{transaction.label}</Text>
+          <Text className="ml-4 text-xl text-white">{transaction.price}</Text>
+          <Text className="ml-4 text-l text-white/40">{transaction.label}</Text>
         </View>
       </TouchableOpacity>
     </View>
