208b86d1ee125d792c6059c274105968ba6389a6
Andrew Chang <21andrewch@gmail.com>
2024-05-13 14:26:21 -0700

    feat: updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
-import { Text, Animated, TouchableOpacity, View } from "react-native";
+import { Text, Animated, TouchableOpacity, View, Image } from "react-native";
 import { useRef, useEffect } from "react";
+import { BlurView } from "expo-blur";
 
 export default function Navigation({
   toggleTransactions,
@@ -8,6 +9,8 @@ export default function Navigation({
   visible,
 }) {
   const fade = useRef(new Animated.Value(1)).current; // Initial value for opacity: 0
+  const history = require("../../assets/history.png");
+  const reciepts = require("../../assets/reciepts.png");
 
   useEffect(() => {
     Animated.timing(fade, {
@@ -18,24 +21,32 @@ export default function Navigation({
   }, [visible, fade]);
   return (
     <Animated.View style={{ opacity: fade }}>
-      <View className="fixed flex-row mb-8">
-        <TouchableOpacity
-          className="flex-1 items-center self-end mx-4 bg-black rounded"
-          onPress={toggleTransactions}
-        >
-          <Text className="p-4 text-white">History</Text>
+      <View className="fixed flex-row mb-6">
+        <TouchableOpacity onPress={toggleTransactions}>
+          <BlurView
+            intensity={30}
+            tint="dark"
+            className="overflow-hidden justify-between items-end p-4 m-4 rounded-full bg-black/80"
+          >
+            <Image source={history} className="w-5 h-5" />
+          </BlurView>
         </TouchableOpacity>
         <TouchableOpacity
-          className="flex-1 items-center self-end mx-4 bg-black rounded"
+          className="flex-1 items-center self-end m-4 bg-black rounded"
           onPress={pickImage}
         >
           <Text className="p-4 text-white">Upload</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          className="flex-1 items-center self-end mx-4 bg-black rounded"
-          onPress={toggleReciepts}
-        >
-          <Text className="p-4 text-white">Reciepts</Text>
+        <TouchableOpacity onPress={toggleReciepts}>
+          <BlurView
+            intensity={30}
+            tint="dark"
+            className="overflow-hidden justify-between items-end p-4 m-4 rounded-full bg-black/80"
+          >
+            <View className="items-center w-5 h-5">
+              <Image source={reciepts} className="w-4 h-5" />
+            </View>
+          </BlurView>
         </TouchableOpacity>
       </View>
     </Animated.View>
